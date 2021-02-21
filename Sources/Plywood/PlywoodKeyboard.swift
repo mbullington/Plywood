419f6a718668b63c06ff2028f15fc10c9a87a97f
Michael Bullington <michael@bullington.xyz>
2021-02-21 17:14:49 -0500

    add naive implementation of super keybindings

--- Sources/Plywood/PlywoodKeyboard.swift
+++ Sources/Plywood/PlywoodKeyboard.swift
@@ -1,6 +1,15 @@
 import SwiftWayland
 import SwiftWLR
 
+enum PWKeyboardModifier {
+    case command
+}
+
+enum PWKeyboardModifierState {
+    case single
+    case multiple
+}
+
 class PlywoodKeyboard {
     let device: WLRInputDevice<WLRKeyboard>
 
@@ -14,6 +23,8 @@ class PlywoodKeyboard {
 
     var modifiersListener: WLListener<WLRKeyboard>!
     var keyListener: WLListener<WLRKeyboardKeyEvent>!
+
+    var modifiers: [PWKeyboardModifier: PWKeyboardModifierState?] = [:]
 
     init(_ device: WLRInputDevice<WLRKeyboard>, state: PlywoodState) {
         self.device = device
@@ -37,7 +48,78 @@ class PlywoodKeyboard {
     }
 
     func onKey(event: WLRKeyboardKeyEvent) {
-        state.seat.seat.notifyKeyboardKey(
-            event.keyCode, state: event.state, time: event.timeInMilliseconds)
+        if !tryHandleKey(event.keyCode, event.state) {
+            state.seat.seat.notifyKeyboardKey(
+                event.keyCode, state: event.state, time: event.timeInMilliseconds)
+        }
     }
+
+    func tryHandleKey(_ keyCode: UInt32, _ keyState: WLRKeyState) -> Bool {
+        // NOTE: We must add 8 to translate from Wayland keycodes to XKB.
+        // https://www.reddit.com/r/swaywm/comments/f96pvw/working_with_keycodes_in_wlroots/firxgxz/?utm_source=reddit&utm_medium=web2x&context=3
+        let sym = keyboard.state.getOneSym(keyCode: keyCode + 8)
+
+        // Handle super key.
+        if sym == XKBKey.Super_L || sym == XKBKey.Super_R || (PlywoodSettings.keyboardCapsLockModifier && sym == XKBKey.Caps_Lock) {            
+            if keyState == .pressed {
+                modifiers[.command] = .single
+            }
+
+            if keyState == .released {
+                if modifiers[.command] == .some(.single) {
+                    commandModifierSingle()
+                }
+                modifiers[.command] = .none
+            }
+
+            return true
+        }
+
+        if (modifiers[.command] != .none && keyState == .pressed) {
+            modifiers[.command] = .multiple
+
+            if (sym == XKBKey.Left || (PlywoodSettings.keyboardWASD && sym == XKBKey.a)) {
+                commandModifierLeft()
+            }
+
+            if (sym == XKBKey.Right || (PlywoodSettings.keyboardWASD && sym == XKBKey.d)) {
+                commandModifierRight()
+            }
+
+            if (sym == XKBKey.Up || (PlywoodSettings.keyboardWASD && sym == XKBKey.w)) {
+                commandModifierUp()
+            }
+
+            if (sym == XKBKey.Down || (PlywoodSettings.keyboardWASD && sym == XKBKey.s)) {
+                commandModifierDown()
+            }
+
+            return true
+        }
+
+        return false
+    }
+
+    // TODO: Implement.
+
+    func commandModifierSingle() {
+        print("single")
+    }
+
+    func commandModifierLeft() {
+        print("left")
+    }
+
+    func commandModifierRight() {
+        print("right")
+    }
+
+    func commandModifierUp() {
+        print("up")
+    }
+
+    func commandModifierDown() {
+        print("down") 
+    }
+
 }
--- Sources/Plywood/PlywoodSettings.swift
+++ Sources/Plywood/PlywoodSettings.swift
@@ -6,4 +6,9 @@ final class PlywoodSettings {
 
     public static let keyboardRate: Int32 = 25
     public static let keyboardDelay: Int32 = 600
+
+    // If true, Plywood will interpret Caps Lock as the Command/"Windows" modifier.    
+    public static let keyboardCapsLockModifier: Bool = true
+    // If true, Plywood can be controlled with Super+WASD in addition to arrow keys.
+    public static let keyboardWASD: Bool = true
 }
