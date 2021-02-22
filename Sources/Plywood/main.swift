06da29260e1fed27cbd471cd9c82d4557bb59480
Michael Bullington <michael@bullington.xyz>
2021-02-21 20:09:16 -0500

    clean up logging

--- Sources/Plywood/main.swift
+++ Sources/Plywood/main.swift
@@ -4,12 +4,10 @@ import Logging
 
 LoggingSystem.bootstrap(WLRLogHandler.init)
 
-let logger = Logger(label: "tinywl")
-
 let server = WaylandServer()
 let state = PlywoodState(for: server)
 
-logger.info("Running Wayland compositor on WAYLAND_DISPLAY=\(server.socket)")
+state.logger.info("Running Wayland compositor on WAYLAND_DISPLAY=\(server.socket)")
 
 // let _ = try! Process.run(
 //     URL(fileURLWithPath: "/bin/sh", isDirectory: false),
